aa0373b6a6625499a3cd509c012de5c6f30bb578
TubaZorlu <tbtstn@gmail.com>
2022-11-01 22:13:46 +0300

    Add project files.

---
+++ Crm.UpSchool.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,24 @@
+﻿using Crm.UpSchool.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crm.UpSchool.DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-CJELTSN\\MSSQLSERVER2019; Database=DbCRM;integrated security=True;");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+ 
+
+
+    }
+}
